a038d4e2b006fa58bc8cac6b422e0148e6fb6061
Omar Choudhury <ochoudhury@aip.de>
2014-07-08 13:27:23 +0200

    Added docstring to SSPlibrary.massWeightedPars

--- lib/ssplibrary.py
+++ lib/ssplibrary.py
@@ -308,6 +308,25 @@ class SSPlibrary(UserDict):
         return numpy.array(mean_out)
 
     def massWeightedPars(self, coefficients, min_age=None, max_age=None):
+        """From the weights of the template spectra in the library, this
+        function computes the parameters by taking mass-weighted averages.
+
+        Parameters
+        ----------
+        coefficients : numpy.ndarray
+            The weights that each template spectrum has.
+        min_age : float, optional
+            Ignore the spectra with ages below a certain threshold.
+        max_age : float, optional
+            Ignore the spectra with ages above a certain threshold.
+
+        Returns
+        -------
+        mean : numpy.ndarray
+            Mass-weighted averages stored in a 1D numpy array. The first
+            value is age, the second value the mass-to-light ratio, the third
+            value [Fe/H], and the fourth value [alpha/Fe]
+        """
         parameters = ['age', 'mass-to-light', '[Fe/H]', '[A/Fe]']
 
         select_age = self['age'] > 0
